77a8d0c1a9bd28ac0b63de114dcbc500f302a7d7
Teodor Levakov <t.levakov@abv.bg>
2022-08-06 20:49:56 +0200

    add detals page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
-const cubes = require('../db.json');
-const fs = require('fs/promises');
-const path = require('path');
-
+const cubeService = require('../services/cubeService.js');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -11,26 +8,25 @@ router.get('/create', (req, res) => {
 router.post('/create', (req, res) => {
     
     let cube = req.body;
-
     //validate
     if (cube.name.length < 2) {
         res.status(400).send('invalid request');
         return;
     };
-
     //save data
-    cubes.push(cube);
-
-    fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {encoding: 'utf-8'})
-        .then(() => {
-            res.redirect('/');
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
-
-
+    cubeService.save(cube)
+    .then(() => {
+        res.redirect('/');
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
     //redirect to page
 });
+
+router.get('/details/:id', (req, res) => {
+    res.render('details');
+});
+
 
 module.exports = router;
