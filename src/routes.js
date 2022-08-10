cd8905954935ca6e3c113342203344b1948a3933
Teodor Levakov <t.levakov@abv.bg>
2022-08-10 15:10:59 +0200

    add accessory page

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,14 @@ const router = express.Router();
 
 const homeController = require('./controllers/homecontroller.js');
 const cubeController = require('./controllers/cubeController.js');
+const accessoryController = require('./controllers/accessoryController.js');
 
 router.use('/', homeController);
 //router.get('/', homeController.index);
 //router.get('/about', homeController.about);
 
 router.use('/cube', cubeController);
-
+router.use('/accessory', accessoryController);
 
 module.exports = router;
 
